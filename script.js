af8b8080f7c2ca459568fc02c047fd496bdbec20
harshan-mana <harshan.23ise@cambridge.edu.in>
2026-03-12 07:55:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ async function shortenMultiple() {
     const longUrl = document.getElementById('longUrl').value;
     const container = document.getElementById('results-container');
     if (!longUrl) return alert("Enter a URL");
-
     container.innerHTML = "Shortening...";
-
-    
     const apis = [
         `https://tinyurl.com/api-create.php?url=${encodeURIComponent(longUrl)}`,
         `https://is.gd/create.php?format=simple&url=${encodeURIComponent(longUrl)}`,
         `https://v.gd/create.php?format=simple&url=${encodeURIComponent(longUrl)}`
     ];
-
     try {
         const results = await Promise.all(apis.map(api => fetch(api).then(res => res.text())));
         container.innerHTML = ""; 
